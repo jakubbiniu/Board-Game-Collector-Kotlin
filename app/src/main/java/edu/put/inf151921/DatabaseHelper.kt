9e24287721d72f4312c84598067f1a27d7e6e1aa
Jakub Binkowski <jakubbiniu@gmail.com>
2023-05-28 22:48:58 +0200

    layout for expasnions and games

--- app/src/main/java/edu/put/inf151921/DatabaseHelper.kt
+++ app/src/main/java/edu/put/inf151921/DatabaseHelper.kt
@@ -9,7 +9,7 @@ class DatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME
 
     companion object {
         private const val DATABASE_NAME = "boardgames.db"
-        private const val DATABASE_VERSION = 1
+        private const val DATABASE_VERSION = 2
 
         const val TABLE_NAME = "games"
         const val COLUMN_ID = "id"
